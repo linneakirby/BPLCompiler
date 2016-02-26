79d178372f7490af70d628d71eb3e3d3007c9b5a
Linnea Kirby <linneakirby@gmail.com>
2016-02-26 00:03:27 -0500

    tests work now (remember newline at end)

--- src/test/BPLParserTest.java
+++ src/test/BPLParserTest.java
@@ -6,14 +6,35 @@ import java.io.File;
 public class BPLParserTest{
 
 	@Test
-	public void x(){
-		BPLParser parse = new BPLParser("x.txt");
-		assertEquals("program {\n\tstatement {\n\t\texpression statement {\n\t\t\texpression {\n\t\t\t\tx {}\n\t\t\t}\n\t\t}\n\t}\n}", parse.toString());
+	public void testX(){
+		try{
+			BPLParser parse = new BPLParser("x.txt");
+			assertEquals("LINE 1: program {\nLINE 1: 	statement {\nLINE 1: 		expression statement {\nLINE 1: 			expression {\nLINE 1: 				x {}\n			}\n		}\n	}\n}\n", parse.toString());
+		}
+		catch(BPLException b){
+			System.out.println(b.getMessage());
+		}
 	}
 
 	@Test
-	public void xyz(){
-		BPLParser parse = new BPLParser("xyz.txt");
-		assertEquals("program {\n\tstatement {\n\t\tcompound statement {\n\t\t\tstatement list {\n\t\t\t\tstatement {\n\t\t\t\t\texpression statement {\n\t\t\t\t\t\texpression {\n\t\t\t\t\t\t\tx {}\n\t\t\t\t\t\t}\n\t\t\t\t\t}\n\t\t\t\t}\n\t\t\t\tstatement list {\n\t\t\t\t\tstatement {\n\t\t\t\t\t\texpression statement {\n\t\t\t\t\t\t\texpression {\n\t\t\t\t\t\t\t\ty {}\n\t\t\t\t\t\t\t}\n\t\t\t\t\t\t}\n\t\t\t\t\t}\n\t\t\t\t\tstatement list {\n\t\t\t\t\t\tstatement {\n\t\t\t\t\t\t\texpression statement {\n\t\t\t\t\t\t\t\texpression {\n\t\t\t\t\t\t\t\t\tz {}\n\t\t\t\t\t\t\t\t}\n\t\t\t\t\t\t\t}\n\t\t\t\t\t\t}\n\t\t\t\t\t\tstatement list {\n\t\t\t\t\t\t\tempty {}\n\t\t\t\t\t\t}\n\t\t\t\t\t}\n\t\t\t\t}\n\t\t\t}\n\t\t}\n\t}\n}", parse.toString());
+	public void testXyz(){
+		try{
+			BPLParser parse = new BPLParser("xyz.txt");
+			assertEquals("LINE 1: program {\nLINE 1: 	statement {\nLINE 1: 		compound statement {\nLINE 1: 			local decs {\nLINE 1: 				empty {}\n			}\nLINE 1: 			statement list {\nLINE 1: 				statement {\nLINE 1: 					expression statement {\nLINE 1: 						expression {\nLINE 1: 							x {}\n						}\n					}\n				}\nLINE 1: 				statement list {\nLINE 1: 					statement {\nLINE 1: 						expression statement {\nLINE 1: 							expression {\nLINE 1: 								y {}\n							}\n						}\n					}\nLINE 1: 					statement list {\nLINE 1: 						statement {\nLINE 1: 							expression statement {\nLINE 1: 								expression {\nLINE 1: 									z {}\n								}\n							}\n						}\nLINE 1: 						statement list {\nLINE 1: 							empty {}\n						}\n					}\n				}\n			}\n		}\n	}\n}\n", parse.toString());
+		}
+		catch(BPLException b){
+			System.out.println(b.getMessage());
+		}
+	}
+
+	@Test
+	public void testIf(){
+		try{
+			BPLParser parse = new BPLParser("if.txt");
+			assertEquals("LINE 1: program {\nLINE 1: 	statement {\nLINE 1: 		compound statement {\nLINE 1: 			local decs {\nLINE 1: 				empty {}\n			}\nLINE 1: 			statement list {\nLINE 1: 				statement {\nLINE 1: 					if statement {\nLINE 1: 						expression {\nLINE 1: 							e {}\n						}\nLINE 1: 						statement {\nLINE 1: 							expression statement {\nLINE 1: 								expression {\nLINE 1: 									s {}\n								}\n							}\n						}\n					}\n				}\nLINE 1: 				statement list {\nLINE 1: 					empty {}\n				}\n			}\n		}\n	}\n}\n", parse.toString());	
+		}
+		catch(BPLException b){
+			System.out.println(b.getMessage());
+		}
 	}
 }
